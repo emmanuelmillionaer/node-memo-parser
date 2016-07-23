2d57b1789d8c56a3203953b7a4d5b311f304991a
Emmanuel Stadler <stadleremmanuel1@gmail.com>
2016-07-23 11:31:59 +0200

    parse block header with record length and signature

--- memo_file.js
+++ memo_file.js
@@ -5,17 +5,34 @@ var MemoFile = function(path){
 	this.next_free_block;
 	this.block_size;
 
+	this.MemoHeaderLength = 512;
+	this.MemoBlockHeaderLength = 8;
+	this.BlockSignatureTypes = { 1: 'text', 0: 'picture'}
+
 	this.info = () => ({ next_free_block: this.next_free_block,
 				  		 block_size: this.block_size });
 
-	this.parseInfo = function(){
-		var buffer = this.read(0,512);
+	this.getFieldInfoAt = (offset) => {
+		var headerStart = offset * this.block_size;
+		var headerEnd = headerStart + this.MemoBlockHeaderLength;
+
+		var buffer = this.read(headerStart,headerEnd);
+
+		var blockSignature = buffer.readUIntBE(0,4);
+		var recordLength = buffer.readUIntBE(4,4);
+
+		return { block_signature: this.BlockSignatureTypes[blockSignature],
+				 record_length: recordLength}
+	};
+
+	this.parseInfo = () => {
+		var buffer = this.read(0,this.MemoHeaderLength);
 
 		this.next_free_block = buffer.readUIntBE(0,4);
 		this.block_size = buffer.readUIntBE(6,2);
 	};
 
-	this.read  = function(start,end){
+	this.read  = (start,end) => {
 		var length = end - start;
 		var buffer = new Buffer(length);
 		var fd = fs.openSync(this.path,'r');
@@ -28,5 +45,6 @@ var MemoFile = function(path){
 	this.parseInfo();
 }
 
-var memoFile = new MemoFile('akr.fpt');
+var memoFile = new MemoFile('../akr.fpt');
 	console.log(memoFile.info());
+	console.log(memoFile.getFieldInfoAt(8));
